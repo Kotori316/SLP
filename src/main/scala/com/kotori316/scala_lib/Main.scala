5d7fdbaeb37e6e5f6a75f86cd49f26fcf5e68421
Kotori316 <Kotori316@gmail.com>
2019-05-05 20:41:28 +0900

    Use cats in Main

--- src/main/scala/com/kotori316/scala_lib/Main.scala
+++ src/main/scala/com/kotori316/scala_lib/Main.scala
@@ -1,5 +1,10 @@
 package com.kotori316.scala_lib
 
+import java.time.ZonedDateTime
+
+import cats.Show
+import cats.implicits._
+
 /**
   * Class to check scala library is included to jar file.
   * This class have nothing with Minecraft.
@@ -10,6 +15,13 @@ object Main {
     val comma = ", "
     val list = 1 :: 3 :: 4 :: 15 :: Nil
     println(s"Sum of ${list.mkString(comma)} = ${list.sum}")
+    println()
+    println(List(ZonedDateTime.now().minusYears(1), ZonedDateTime.now(), ZonedDateTime.now().plusYears(1)).show)
+    println()
     println("-" * 34)
+  }
+
+  implicit val DateShow: Show[ZonedDateTime] = time => {
+    s"${time.getYear} ${time.getMonthValue}/${time.getDayOfMonth} ${time.getDayOfWeek}"
   }
 }
