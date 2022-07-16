34d67544ba83f00678668fcc3d9a5ba86f3665be
Kotori316 <Kotori316@gmail.com>
2022-07-16 17:00:44 +0900

    Remove util classes.

--- src/main/scala/com/kotori316/scala_lib/example/SLPTest.java
+++
@@ -1,8 +0,0 @@
-package com.kotori316.scala_lib.example;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(SLPTest.MOD_ID)
-class SLPTest {
-    public static final String MOD_ID = "slp_test";
-}
