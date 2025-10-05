d88ebf1e8cd5c85bcf8755c667ac7d809083bbe2
Kotori316 <Kotori316@gmail.com>
2025-10-06 00:26:32 +0900

    Add resource files

--- neoforge-1.21.9/build.gradle.kts
+++ neoforge-1.21.9/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     id("com.kotori316.common.java")
     id("com.kotori316.common.publish")
@@ -51,3 +54,109 @@ neoForge {
         enable()
     }
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "FMLModType" to "LIBRARY",
+            "Automatic-Module-Name" to "kotori_scala",
+        )
+        attributes(
+            "Specification-Title" to project.name,
+            "Specification-Vendor" to "Kotori316",
+            "Specification-Version" to "1", // We are version 1 of ourselves
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "Kotori316",
+            "Implementation-Timestamp" to ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT),
+        )
+    }
+    archiveClassifier = "dev"
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    inputs.property("version", project.version)
+    filesMatching("version.txt") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.register("normalJar", org.gradle.jvm.tasks.Jar::class) {}
+
+artifacts {
+    archives(tasks.shadowJar)
+    archives(tasks.sourcesJar)
+}
+
+tasks.register("jksSignJar") {
+    dependsOn(tasks.shadowJar, tasks.jar, tasks.sourcesJar)
+    val executeCondition = project.hasProperty("jarSign.keyAlias") &&
+            project.hasProperty("jarSign.keyLocation") &&
+            project.hasProperty("jarSign.storePass")
+    onlyIf { executeCondition }
+    doLast {
+        listOf(tasks.shadowJar, tasks.jar, tasks.sourcesJar).forEach { t ->
+            ant.withGroovyBuilder {
+                "signjar"(
+                    "jar" to t.get().archiveFile.get(),
+                    "alias" to project.findProperty("jarSign.keyAlias"),
+                    "keystore" to project.findProperty("jarSign.keyLocation"),
+                    "storepass" to project.findProperty("jarSign.storePass"),
+                    "sigalg" to "Ed25519",
+                    "digestalg" to "SHA-256",
+                    "tsaurl" to "http://timestamp.digicert.com",
+                )
+            }
+        }
+    }
+}
+
+tasks.named("assemble") {
+    dependsOn("jksSignJar")
+}
+
+signing {
+    sign(publishing.publications)
+    sign(tasks.jar.get(), tasks.shadowJar.get(), tasks.sourcesJar.get())
+}
+
+val hasGpgSignature = project.hasProperty("signing.keyId") &&
+        project.hasProperty("signing.password") &&
+        project.hasProperty("signing.secretKeyRingFile")
+
+tasks.withType(Sign::class) {
+    onlyIf {
+        hasGpgSignature
+    }
+}
+
+tasks.withType(AbstractPublishToMaven::class) {
+    if (hasGpgSignature) {
+        dependsOn(":neoforge-1.21.9:signJar")
+        dependsOn(":neoforge-1.21.9:signSourcesJar")
+        dependsOn(":neoforge-1.21.9:signShadowJar")
+    }
+}
+
+fun createChangelog(): String {
+    val t = """
+        For Minecraft 1.21.9
+        
+        Built with forge ${libs.versions.neo1219.get()}
+        
+        This mod provides language provider, "kotori_scala".
+        
+        Scala3: ${libs.versions.scala3.get()}
+        Scala: ${libs.versions.scala2.get()}
+        Cats: ${libs.versions.cats.get()}
+        """.trimIndent()
+    return t
+}
+
+afterEvaluate {
+    rootProject.tasks.named("githubRelease") { dependsOn(":neoforge-1.21.9:assemble") }
+}
+
+ext["archivesBaseName"] = base.archivesName.get()
+ext["generalDescription"] = createChangelog()
