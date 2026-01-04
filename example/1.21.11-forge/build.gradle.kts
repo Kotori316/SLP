de99092c1f14b9a6095caa2f8f5b528cc3b448f3
Kotori316 <Kotori316@gmail.com>
2026-01-05 00:24:24 +0900

    Add example modules for Minecraft 1.21.11

---
+++ example/1.21.11-forge/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    scala
+    idea
+    alias(libs.plugins.forge.gradle)
+    alias(libs.plugins.parchment.librarian)
+}
+
+base {
+    archivesName = "slp_example_1.21.11"
+}
+
+version = "1.0.0"
+group = "com.kotori316.slp.examples"
+
+minecraft {
+    mappings(
+        mapOf(
+            "channel" to "official",
+            "version" to "1.21.11",
+        )
+    )
+    reobf = false
+    copyIdeResources = true
+
+    runs {
+        configureEach {
+            workingDirectory = "./run"
+            property("forge.logging.markers", "REGISTRIES")
+            property("forge.logging.console.level", "info")
+        }
+
+        // Forge 1.21.5 does not support game test server, so run data generator instead.
+        create("data") {
+            args(
+                "--mod",
+                "slp_examples",
+                "--all",
+                "--output",
+                file("run/resources/"),
+                "--existing",
+                file("src/main/resources/")
+            )
+        }
+    }
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        name = "Minecraft-Manually"
+        url = uri("https://libraries.minecraft.net/")
+        content {
+            includeGroup("org.lwjgl")
+            includeGroup("com.mojang")
+        }
+    }
+    mavenCentral()
+    maven {
+        name = "Kotori316"
+        url = uri("https://maven.kotori316.com")
+        content {
+            includeModule("org.typelevel", "cats-core_3")
+            includeModule("org.typelevel", "cats-kernel_3")
+            includeModule("org.typelevel", "cats-free_3")
+        }
+    }
+}
+
+dependencies {
+    minecraft(libs.forge12111)
+    compileOnly(libs.scala3)
+    // implementation(project(":forge"))
+    runtimeOnly(project(":forge-1.21.6")) {
+        isTransitive = false
+    }
+    implementation("net.sf.jopt-simple:jopt-simple:5.0.4") {
+        version {
+            strictly("5.0.4")
+        }
+    }
+}
+
+sourceSets.forEach {
+    val dir = layout.buildDirectory.dir("sourcesSets/${it.name}")
+    it.output.setResourcesDir(dir)
+    it.java.destinationDirectory = dir
+    it.scala.destinationDirectory = dir
+}
+
+tasks.compileScala {
+    dependsOn(tasks.processResources)
+}
