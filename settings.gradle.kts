4c8b7565dafdfe79b31e2b7a913293b984108f09
Kotori316 <Kotori316@gmail.com>
2026-03-01 15:11:26 +0900

    Drop check of old version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,12 +38,12 @@ if (!System.getenv("DISABLE_NEO_FORGE").toBoolean()) {
 }
 if(!System.getenv("DISABLE_EXAMPLE").toBoolean()) {
     if (!System.getenv("DISABLE_FORGE").toBoolean()) {
-        include("forge")
-        include("example:1.21.1-forge")
-        include("example:1.21.3-forge")
-        include("example:1.21.4-forge")
-        include("example:1.21.5-forge")
-        include("example:1.21.6-forge")
+        // include("forge")
+        // include("example:1.21.1-forge")
+        // include("example:1.21.3-forge")
+        // include("example:1.21.4-forge")
+        // include("example:1.21.5-forge")
+        // include("example:1.21.6-forge")
         include("example:1.21.9-forge")
         include("example:1.21.11-forge")
     }
