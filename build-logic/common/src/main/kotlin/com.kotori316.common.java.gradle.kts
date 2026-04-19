6eac06d6767e014e0d9e8b206eacb7dfe46dfd30
Kotori316 <Kotori316@gmail.com>
2026-04-19 17:05:12 +0900

    Data?

--- build-logic/common/src/main/kotlin/com.kotori316.common.java.gradle.kts
+++ build-logic/common/src/main/kotlin/com.kotori316.common.java.gradle.kts
@@ -63,51 +63,11 @@ tasks.test {
     jvmArgs.add("-javaagent:${mockitoAgent.asPath}")
 }
 
-fun inGroup(name: String, action: () -> Unit) {
-    println("::group::${name}")
-    action()
-    println("::endgroup::")
-}
-
 tasks.register("data") {
     doLast {
         println(
             "Java: ${System.getProperty("java.version")} JVM: ${System.getProperty("java.vm.version")}" +
                     "(${System.getProperty("java.vendor")}) Arch: ${System.getProperty("os.arch")}"
         )
-        println(
-            """
-            Scala: ${catalog.findVersion("scala3").map { it.requiredVersion }.get()}
-            Build Number: ${project.property("build_number")}, ${project.property("build_number")?.javaClass}
-            Version: ${project.version}, ${project.version.javaClass}
-            Minecraft: ${catalog.findVersion("minecraft").map { it.requiredVersion }.get()}
-            Forge: ${catalog.findVersion("forge").map { it.requiredVersion }.get()}
-            NeoForge: ${catalog.findVersion("neoforge").map { it.requiredVersion }.get()}
-        """.trimIndent()
-        )
-        print(System.lineSeparator().repeat(2))
-        println("All Dependencies")
-        project.configurations.forEach { con ->
-            if (con.dependencies.isNotEmpty()) {
-                inGroup(con.name) {
-                    con.dependencies.forEach { o -> println(o) }
-                }
-            }
-        }
-        print(System.lineSeparator().repeat(2))
-        inGroup("Dependencies to be included in pom") {
-            project.configurations["implementation"].dependencies.forEach { dep ->
-                if (!dep.name.contains("junit")) {
-                    println("Group: %s, ArtifactId: %s, Version: %s".format(dep.group, dep.name, dep.version))
-                } else {
-                    println("JUNIT - Group: %s, ArtifactId: %s, Version: %s".format(dep.group, dep.name, dep.version))
-                }
-            }
-        }
-        inGroup("Publishing Information") {
-            project.publishing.repositories.asMap.forEach { (name, value) ->
-                println("$name -> $value")
-            }
-        }
     }
 }
