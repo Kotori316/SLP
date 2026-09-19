dc2aa3b0dec1655519579287ee0782645a1e57d1
Kotori316 <Kotori316@gmail.com>
2026-09-19 13:09:20 +0900

    Fix syntax [no ci]

--- build-logic/common/build.gradle.kts
+++ build-logic/common/build.gradle.kts
@@ -13,6 +13,6 @@ dependencies {
         "com.kotori316.plugin.cf" to libs.versions.plugin.cf.get(),
         "com.gradleup.shadow" to libs.versions.plugin.shadow.get(),
     ).forEach { (name, version) ->
-        implementation(group = name, name = "${name}.gradle.plugin", version = version)
+        implementation("$name:${name}.gradle.plugin:$version")
     }
 }
